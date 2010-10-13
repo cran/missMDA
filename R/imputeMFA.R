b33d2a14bf4ba6654a08eda46e4ffc825bfbd31c
Francois Husson <husson@agrocampus-ouest.fr>
2010-10-13 00:00:00 +0000

    version 1.2

--- R/imputeMFA.R
+++ R/imputeMFA.R
@@ -1,6 +1,6 @@
-imputePCA <- function (X, ncp = 2, scale=TRUE, method="Regularized",threshold = 1e-6,seed = NULL,nb.init=1,maxiter=1000,...){
+imputeMFA <- function (X, group, ncp = 2, scale=TRUE, method="Regularized",threshold = 1e-6,seed = NULL,nb.init=1,maxiter=1000,...){
 
-impute <- function (X, ncp = 4, scale=TRUE, method=NULL,threshold = 1e-6,seed = NULL,init=1,maxiter=1000,...){
+impute <- function (X, group, ncp = 4, scale=TRUE, method=NULL,threshold = 1e-6,seed = NULL,init=1,maxiter=1000,...){
 
    if (!is.null(seed)) set.seed(seed)
    X <- as.matrix(X)
@@ -16,17 +16,24 @@ impute <- function (X, ncp = 4, scale=TRUE, method=NULL,threshold = 1e-6,seed = 
    if (any(is.na(X))) Xhat[missing] <- 0
    if (init>1) Xhat[missing] <- rnorm(length(missing)) ## random initialization
    recon <- Xhat
-
+ ponderation = rep(nbr/svd(scale(Xhat[,1:group[1]],scale=FALSE),nu=1,nv=1)$d[1]^2,group[1])
+       for (i in 2:length(group)) ponderation = c(ponderation,rep(nbr/svd(scale(Xhat[,(sum(group[1:(i-1)])+1):sum(group[1:i])],scale=FALSE),nu=1,nv=1)$d[1]^2,group[i]))
+ Xhat <- sweep(Xhat, 2, sqrt(ponderation), FUN = "*")
+ 
    nb.iter <- 1
    old <- Inf
    while (nb.iter > 0) {
        Xhat[missing] <- recon[missing]
-       if (scale) Xhat=sweep(Xhat,2,et, FUN="*")
+       Xhat <- sweep(Xhat, 2, sqrt(ponderation), FUN = "/")
+       if (scale) Xhat <- sweep(Xhat,2,et, FUN="*")
        Xhat <- sweep(Xhat,2,moy.p, FUN="+")
        moy.p <- apply(Xhat, 2, mean,na.rm=TRUE)
        et <- apply(Xhat, 2, sd,na.rm=TRUE)
-       Xhat <- sweep(Xhat,2,moy.p, FUN="-")
-       if (scale) Xhat <- sweep(Xhat,2,et, FUN="/")
+       Xhat=sweep(Xhat,2,moy.p, FUN="-")
+       if (scale) Xhat=sweep(Xhat,2,et, FUN="/")
+       ponderation = rep(nbr/svd(scale(Xhat[,1:group[1]],scale=FALSE),nu=1,nv=1)$d[1]^2,group[1])
+       for (i in 2:length(group)) ponderation = c(ponderation,rep(nbr/svd(scale(Xhat[,(sum(group[1:(i-1)])+1):sum(group[1:i])],scale=FALSE),nu=1,nv=1)$d[1]^2,group[i]))
+       Xhat = sweep(Xhat,2,sqrt(ponderation),FUN="*")
 
        svd.res <- svd.triplet(scale(Xhat,scale=FALSE))
        sigma2 <- mean(svd.res$vs[-(1:ncp)]^2)
@@ -52,6 +59,7 @@ impute <- function (X, ncp = 4, scale=TRUE, method=NULL,threshold = 1e-6,seed = 
          warning(paste("Stopped after ",maxiter," iterations"))
        }
    }
+   Xhat = sweep(Xhat,2,sqrt(ponderation),"/")
    if (scale) Xhat <- sweep(Xhat,2,et, FUN="*")
    Xhat <- sweep(Xhat,2,moy.p, FUN="+")
    completeObs <- X
@@ -72,7 +80,7 @@ impute <- function (X, ncp = 4, scale=TRUE, method=NULL,threshold = 1e-6,seed = 
  if (ncp>=min(nrow(X)-2,ncol(X)-1)) stop("ncp is too large")
  for (i in 1:nb.init){
   if (!any(is.na(X))) return(X)
-  res.impute=impute(X, ncp=ncp, scale=scale, method=method, threshold = threshold,seed=seed,init=i,maxiter=maxiter)
+  res.impute=impute(X, group=group, ncp=ncp, scale=scale, method=method, threshold = threshold,seed=seed,init=i,maxiter=maxiter)
   if (mean((res.impute$recon[!is.na(X)]-X[!is.na(X)])^2) < obj){
     res <- res.impute
     obj <- mean((res.impute$recon[!is.na(X)]-X[!is.na(X)])^2)
