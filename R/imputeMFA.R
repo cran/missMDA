27faf020f2d5e4830c3ca75ee6923194c1392f27
Francois Husson <husson@agrocampus-ouest.fr>
2012-08-03 08:14:19 +0000

    version 1.5

--- R/imputeMFA.R
+++ R/imputeMFA.R
@@ -1,91 +1,223 @@
-imputeMFA <- function (X, group, ncp = 2, scale=TRUE, method="Regularized",threshold = 1e-6,seed = NULL,nb.init=1,maxiter=1000,...){
+imputeMFA <- function (X, group, ncp = 2, type=rep("s",length(group)), method="Regularized",row.w=NULL,coeff.ridge=1,threshold = 1e-6,seed = NULL,nb.init=1,maxiter=1000,...){
 
-impute <- function (X, group, ncp = 4, scale=TRUE, method=NULL,threshold = 1e-6,seed = NULL,init=1,maxiter=1000,...){
+impute <- function (X, group, ncp = 2, type=rep("s",length(group)), method=NULL,threshold = 1e-6,seed = NULL,init=1,maxiter=1000,row.w=NULL,coeff.ridge=1,...){
+    moy.p <- function(V, poids) {
+        res <- sum(V * poids,na.rm=TRUE)/sum(poids[!is.na(V)])
+    }
+    ec <- function(V, poids) {
+        res <- sqrt(sum(V^2 * poids,na.rm=TRUE)/sum(poids[!is.na(V)]))
+    }
+    tab.disjonctif.NA <- function(tab) {
+        tab <- as.data.frame(tab)
+        modalite.disjonctif <- function(i) {
+            moda <- tab[, i]
+            nom <- names(tab)[i]
+            n <- length(moda)
+            moda <- as.factor(moda)
+            x <- matrix(0, n, length(levels(moda)))
+            ind <- (1:n) + n * (unclass(moda) - 1)
+            indNA <- which(is.na(ind))
+            x[(1:n) + n * (unclass(moda) - 1)] <- 1
+            x[indNA, ] <- NA
+            if ((ncol(tab) != 1) & (levels(moda)[1] %in% c(1:nlevels(moda), 
+                "n", "N", "y", "Y"))) 
+                dimnames(x) <- list(row.names(tab), paste(nom, 
+                  levels(moda), sep = "."))
+            else dimnames(x) <- list(row.names(tab), levels(moda))
+            return(x)
+        }
+        if (ncol(tab) == 1) 
+            res <- modalite.disjonctif(1)
+        else {
+            res <- lapply(1:ncol(tab), modalite.disjonctif)
+            res <- as.matrix(data.frame(res, check.names = FALSE))
+        }
+        return(res)
+    }
 
+find.category <- function (X,tabdisj){
+  nbdummy <- rep(1,ncol(X))
+  is.quali <- which(!unlist(lapply(X,is.numeric)))
+  nbdummy[is.quali] <- unlist(lapply(X[,is.quali,drop=FALSE],nlevels))
+  vec = c(0,cumsum(nbdummy))
+  Xres <- X
+  for (i in 1:ncol(X)){
+    if (i %in% is.quali) Xres[,i] <- as.factor(levels(X[,i])[apply(tabdisj[,(vec[i]+1):vec[i+1]],1,which.max)])
+	else Xres[,i] <- tabdisj[,i]
+  }
+  return(Xres)
+}
+
+############# main program
    if (!is.null(seed)) set.seed(seed)
-   X <- as.matrix(X)
-   nbr <- nrow(X)
-   nbc <- ncol(X)
-   ncp <- min(ncp,nbc,nbr-1)
-   Obs <- !is.na(X)
-   missing <- which(is.na(X))
-   moy.p <- apply(X, 2, mean,na.rm=TRUE)
-   et <- apply(X, 2, sd,na.rm=TRUE)
-   Xhat <- sweep(X, 2,moy.p,FUN="-")
-   if (scale) Xhat <- sweep(Xhat, 2,et,FUN="/")
-   if (any(is.na(X))) Xhat[missing] <- 0
-   if (init>1) Xhat[missing] <- rnorm(length(missing)) ## random initialization
-   recon <- Xhat
- ponderation = rep(nbr/svd(scale(Xhat[,1:group[1]],scale=FALSE),nu=1,nv=1)$d[1]^2,group[1])
-       for (i in 2:length(group)) ponderation = c(ponderation,rep(nbr/svd(scale(Xhat[,(sum(group[1:(i-1)])+1):sum(group[1:i])],scale=FALSE),nu=1,nv=1)$d[1]^2,group[i]))
- Xhat <- sweep(Xhat, 2, sqrt(ponderation), FUN = "*")
- 
+   if ("n"%in%type){
+      niveau = NULL
+      for (j in 1:ncol(X)){
+        if (!is.numeric(X[,j])) niveau = c(niveau,levels(X[,j]))
+      }
+      for (j in 1:ncol(X)) {
+        if (!is.numeric(X[,j])){
+          if (sum(niveau%in%levels(X[,j]))!=nlevels(X[,j])) levels(X[,j]) = paste(colnames(X)[j],levels(X[,j]),sep="_")
+        }
+      }
+    }
+  if (is.null(row.w)) row.w=rep(1/nrow(X),nrow(X))
+	group.mod=group
+    Xhat <- matrix(0,nrow(X),0)
+    Xhat2 <- matrix(0,nrow(X),0)
+	MM <- vector(mode = "list", length = length(group)) ## vecteur de moyenne ou de marge selon quanti ou quali
+	ET <- vector(mode = "list", length = length(group))
+	tab.disj.comp <- vector(mode = "list", length = length(group))
+	ponderation <- rep(1,length(group))
+	for (g in 1:length(group)){
+	    if (g==1) aux.base <- X[,1:group[1],drop=FALSE]
+		else aux.base <- X[,(cumsum(group)[g-1]+1):cumsum(group)[g],drop=FALSE]
+        if (type[g] == "s") {
+          Xhat2 <- cbind.data.frame(Xhat2, aux.base)
+		  MM[[g]] <- apply(as.data.frame(aux.base), 2, moy.p, row.w)
+          aux.base <- as.matrix(sweep(as.data.frame(aux.base), 2, MM[[g]], FUN = "-"))
+          ET[[g]] <- apply(as.data.frame(aux.base), 2, ec, row.w)
+#          ET[[g]][ET[[g]] <= 1e-08] <- 1
+          aux.base <- as.matrix(sweep(as.data.frame(aux.base), 2, ET[[g]], FUN = "/"))
+          missing <- which(is.na(as.matrix(aux.base)))
+          if (any(is.na(aux.base))) aux.base[missing] <- 0
+          ponderation[g] <- svd.triplet(aux.base,ncp=1,row.w=row.w)$vs[1]
+		  Xhat <- cbind.data.frame(Xhat, aux.base/ponderation[g])
+          if (init>1) Xhat[missing] <- rnorm(length(missing)) ## random initialization
+        }
+        if (type[g] == "c") {
+          Xhat2 <- cbind.data.frame(Xhat2, aux.base)
+		  MM[[g]] <- apply(as.data.frame(aux.base), 2, moy.p,row.w)
+          aux.base <- as.matrix(sweep(as.data.frame(aux.base), 2, MM[[g]], FUN = "-"))
+          missing <- which(is.na(as.matrix(aux.base)))
+          if (any(is.na(aux.base))) aux.base[missing] <- 0          
+          ponderation[g] = svd.triplet(aux.base,ncp=1,row.w=row.w)$vs[1]		  
+		  Xhat <- cbind.data.frame(Xhat, aux.base/ponderation[g])
+          if (init>1) Xhat[missing] <- rnorm(length(missing)) ## random initialization
+        }
+        if (type[g] == "n") {
+		   tab.disj = tab.disjonctif.prop(aux.base,row.w=row.w)
+		   tab.disj.comp[[g]] = tab.disj
+		   group.mod[g] <- ncol(tab.disj)
+           MM[[g]] = apply(tab.disj, 2, moy.p,row.w)/ncol(aux.base)
+           Z = sweep(tab.disj, 2, apply(tab.disj, 2, moy.p,row.w), FUN = "/")
+           Z = sweep(Z, 2,apply(Z,2,moy.p,row.w),FUN="-")
+	       Zscale = sweep(Z, 2, sqrt(MM[[g]]), FUN = "*")
+           ponderation[g] <- svd.triplet(Zscale,row.w=row.w)$vs[1]
+		   Xhat <- cbind.data.frame(Xhat, Zscale/ponderation[g])
+           Xhat2 <- cbind.data.frame(Xhat2, as.data.frame(tab.disjonctif.NA(aux.base)))
+        }
+	}
+   ind.var <- vector(mode = "list", length = length(group))
+   ind.var[[1]] <- 1:group.mod[1]
+   for (g in 2:length(group)) ind.var[[g]] <- (cumsum(group.mod)[g-1]+1):cumsum(group.mod)[g]
+   recon <- Xhat <- as.matrix(Xhat)
+   if (ncp>=min(nrow(Xhat)-2,ncol(Xhat)-1)) stop("ncp is too large")
+   ncp <- min(ncp,ncol(X),nrow(X)-1)
+   missing <- which(is.na(as.matrix(Xhat2)))
    nb.iter <- 1
    old <- Inf
    while (nb.iter > 0) {
-       Xhat[missing] <- recon[missing]
-       Xhat <- sweep(Xhat, 2, sqrt(ponderation), FUN = "/")
-       if (scale) Xhat <- sweep(Xhat,2,et, FUN="*")
-       Xhat <- sweep(Xhat,2,moy.p, FUN="+")
-       moy.p <- apply(Xhat, 2, mean,na.rm=TRUE)
-       et <- apply(Xhat, 2, sd,na.rm=TRUE)
-       Xhat=sweep(Xhat,2,moy.p, FUN="-")
-       if (scale) Xhat=sweep(Xhat,2,et, FUN="/")
-       ponderation = rep(nbr/svd(scale(Xhat[,1:group[1]],scale=FALSE),nu=1,nv=1)$d[1]^2,group[1])
-       for (i in 2:length(group)) ponderation = c(ponderation,rep(nbr/svd(scale(Xhat[,(sum(group[1:(i-1)])+1):sum(group[1:i])],scale=FALSE),nu=1,nv=1)$d[1]^2,group[i]))
-       Xhat = sweep(Xhat,2,sqrt(ponderation),FUN="*")
-
-       svd.res <- svd.triplet(scale(Xhat,scale=FALSE))
-       sigma2 <- mean(svd.res$vs[-(1:ncp)]^2)
-       if (method=="em") sigma2 <-0
-
-       if (ncp==1) {
-         lambda.shrinked=(svd.res$vs[1]^2-sigma2)/sqrt(svd.res$vs[1]^2)
-         recon=(svd.res$U[,1]%*%diag(lambda.shrinked[1],1)%*%(t(svd.res$V[,1])))
-       } else {
-         lambda.shrinked=diag((svd.res$vs[1:ncp]^2-sigma2)/sqrt(svd.res$vs[1:ncp]^2))
-         recon=(svd.res$U[,1:ncp]%*%lambda.shrinked%*% (t(svd.res$V[,1:ncp])))
+     Xhat[missing] <- recon[missing]
+ 	 for (g in 1:length(group)){
+       if (g==1) aux.base <- Xhat[,1:group.mod[1],drop=FALSE]
+	   else aux.base <- Xhat[,(cumsum(group.mod)[g-1]+1):cumsum(group.mod)[g],drop=FALSE]
+ 	   aux.base <- aux.base*ponderation[g]
+       if (type[g] == "s") {
+		  aux.base <- sweep(aux.base,2,ET[[g]],FUN="*")
+		  aux.base <- sweep(aux.base,2,MM[[g]],FUN="+")
+		  MM[[g]] <- apply(aux.base, 2, moy.p,row.w)
+		  aux.base <- sweep(aux.base,2,MM[[g]], FUN="-")
+		  ET[[g]] <- apply(aux.base, 2, ec,row.w)
+		  aux.base <- sweep(aux.base,2,ET[[g]], FUN="/")
+		  ponderation[g] = svd.triplet(aux.base,ncp=1,row.w=row.w)$vs[1]
        }
+       if (type[g] == "c") {
+		  aux.base <- sweep(aux.base,2,MM[[g]],FUN="+")
+		  MM[[g]] <- apply(aux.base, 2, moy.p,row.w)
+		  aux.base <- sweep(aux.base,2,MM[[g]], FUN="-")
+		  ponderation[g] = svd.triplet(aux.base,ncp=1,row.w=row.w)$vs[1]
+       }
+       if (type[g] == "n") {
+          tab.disj = sweep(aux.base,2,sqrt(MM[[g]]),FUN="/") + matrix(1,nrow(aux.base),ncol(aux.base)) 
+          tab.disj = sweep(tab.disj,2,apply(tab.disj.comp[[g]],2,moy.p,row.w),FUN="*")
+          tab.disj.comp[[g]] = tab.disj
+          MM[[g]] = apply(tab.disj, 2, moy.p,row.w)/ncol(aux.base)
+		  if (any(MM[[g]]<0)) stop(paste("The algorithm fails to converge. Choose a number of components (ncp) less or equal than ",ncp-1," or a number of iteratsions (maxiter) less or equal than ",maxiter-1,sep=""))
+          Z = sweep(tab.disj, 2, apply(tab.disj, 2, moy.p,row.w), FUN = "/")
+          Z = sweep(Z, 2,apply(Z,2,moy.p,row.w),FUN="-")
+	      aux.base = sweep(Z, 2, sqrt(MM[[g]]), FUN = "*")
+          ponderation[g] <- svd.triplet(aux.base,row.w=row.w)$vs[1]
+       }
+	   if (g==1) Xhat[,1:group.mod[1]] <- aux.base / ponderation[g]
+	   else Xhat[,(cumsum(group.mod)[g-1]+1):cumsum(group.mod)[g]] <- aux.base / ponderation[g]
+	}
+	 svd.res <- svd.triplet(Xhat,row.w=row.w)
+     sigma2 <- mean(svd.res$vs[-(1:ncp)]^2)
+     sigma2 <- min(sigma2*coeff.ridge,svd.res$vs[ncp+1]^2)
+	 
+     if (method=="em") sigma2 <-0
+     lambda.shrinked=(svd.res$vs[1:ncp]^2-sigma2)/svd.res$vs[1:ncp]
+     if (ncp==1) recon=tcrossprod(sweep(svd.res$U[,1,drop=FALSE],1,row.w,FUN="*")*lambda.shrinked,svd.res$V[,1,drop=FALSE])
+     else recon=tcrossprod(sweep(sweep(svd.res$U[,1:ncp],1,row.w,FUN="*"),2,lambda.shrinked,FUN="*"),svd.res$V[,1:ncp])
+     recon <- sweep(recon,1,row.w,FUN="/")
 
-       objective <- mean((Xhat[-missing]-recon[-missing])^2)
-       criterion <- abs(1 - objective/old)
-       old <- objective
-       nb.iter <- nb.iter + 1
-       if (!is.nan(criterion)) {
-         if ((criterion < threshold) && (nb.iter > 5))  nb.iter <- 0
-       }
-       if (nb.iter>maxiter) {
-         nb.iter <- 0
-         warning(paste("Stopped after ",maxiter," iterations"))
-       }
+	 diff <- Xhat-recon
+	 diff[missing] <- 0
+     objective <- sum(sweep(diff^2,1,row.w,FUN="*"))
+     criterion <- abs(1 - objective/old)
+     old <- objective
+     nb.iter <- nb.iter + 1
+     if (!is.nan(criterion)) {
+       if ((criterion < threshold) && (nb.iter > 5))  nb.iter <- 0
+       if ((objective < threshold) && (nb.iter > 5))  nb.iter <- 0
+     }
+     if (nb.iter>maxiter) {
+       nb.iter <- 0
+       warning(paste("Stopped after ",maxiter," iterations"))
+     }
    }
-   Xhat = sweep(Xhat,2,sqrt(ponderation),"/")
-   if (scale) Xhat <- sweep(Xhat,2,et, FUN="*")
-   Xhat <- sweep(Xhat,2,moy.p, FUN="+")
-   completeObs <- X
+### end of the loop
+	for (g in 1:length(group)){
+	    if (g==1) aux.base <- Xhat[,1:group.mod[1],drop=FALSE]
+		else aux.base <- Xhat[,(cumsum(group.mod)[g-1]+1):cumsum(group.mod)[g],drop=FALSE]
+		aux.base <- aux.base*ponderation[g]
+        if (type[g] == "s") {
+		  aux.base <- sweep(aux.base,2,ET[[g]],FUN="*")
+		  aux.base <- sweep(aux.base,2,MM[[g]],FUN="+")
+        }
+        if (type[g] == "c")  aux.base <- sweep(aux.base,2,MM[[g]],FUN="+")
+        if (type[g] == "n") {
+          tab.disj = sweep(aux.base,2,sqrt(MM[[g]]),FUN="/") + matrix(1,nrow(aux.base),ncol(aux.base)) 
+##          aux.base = sweep(sweep(tab.disj,1,row.w,FUN="*"),2,apply(tab.disj.comp[[g]],2,sum),FUN="*")
+          aux.base = sweep(tab.disj,2,apply(tab.disj.comp[[g]],2,moy.p,row.w),FUN="*")
+		  }
+		if (g==1) Xhat[,1:group.mod[1]] <- aux.base
+		else Xhat[,(cumsum(group.mod)[g-1]+1):cumsum(group.mod)[g]] <- aux.base
+	}
+   completeObs <- as.matrix(Xhat2)
    completeObs[missing] <- Xhat[missing]
-   if (scale) recon <- sweep(recon,2,et, FUN="*")
-   recon <- sweep(recon,2,moy.p, FUN="+")
 
    result <- list()
-   result$completeObs <- completeObs
-   result$objective <- objective
-   result$recon <- recon
+   result$tab.disj <- completeObs
+   result$completeObs <- find.category(X,completeObs)
    return(result) 
 }
 
 #### Main program
  obj=Inf
  method <- tolower(method)
- if (ncp>=min(nrow(X)-2,ncol(X)-1)) stop("ncp is too large")
- for (i in 1:nb.init){
-  if (!any(is.na(X))) return(X)
-  res.impute=impute(X, group=group, ncp=ncp, scale=scale, method=method, threshold = threshold,seed=seed,init=i,maxiter=maxiter)
-  if (mean((res.impute$recon[!is.na(X)]-X[!is.na(X)])^2) < obj){
-    res <- res.impute
-    obj <- mean((res.impute$recon[!is.na(X)]-X[!is.na(X)])^2)
-  }
- }
-return(res)
+ if (is.null(row.w)) row.w = rep(1,nrow(X))/nrow(X)
+# for (i in 1:nb.init){
+i=1
+  if (!any(is.na(X))) stop("no missing values in X, this function is not useful. Perform MFA on X.")
+  res.impute <- impute(X, group=group,ncp=ncp, type=type, method=method, threshold = threshold,seed=seed,init=i,maxiter=maxiter,row.w=row.w,coeff.ridge=coeff.ridge)
+return(res.impute)
+#  if (mean((res.impute$recon[!is.na(X)]-X[!is.na(X)])^2) < obj){
+#    res <- res.impute
+#    obj <- mean((res.impute$recon[!is.na(X)]-X[!is.na(X)])^2)
+#  }
+# }
+#return(res)
 }
 
---
+++ missMDA-Ex.R
@@ -0,0 +1,227 @@
+pkgname <- "missMDA"
+source(file.path(R.home("share"), "R", "examples-header.R"))
+options(warn = 1)
+options(pager = "console")
+library('missMDA')
+
+assign(".oldSearch", search(), pos = 'CheckExEnv')
+cleanEx()
+nameEx("MIPCA")
+### * MIPCA
+
+flush(stderr()); flush(stdout())
+
+### Name: MIPCA
+### Title: Multiple Imputation with PCA
+### Aliases: MIPCA
+### Keywords: multivariate
+
+### ** Examples
+
+data(orange)
+## First the number of components has to be chosen 
+##   (for the reconstruction step)
+## nb <- estim_ncpPCA(orange,ncp.max=5) ## Time consuming, nb = 2
+
+## Multiple Imputation
+resMI <- MIPCA(orange,ncp=2)
+
+## Visualization on the PCA map
+plot(resMI)
+
+
+
+cleanEx()
+nameEx("estim_ncpMCA")
+### * estim_ncpMCA
+
+flush(stderr()); flush(stdout())
+
+### Name: estim_ncpMCA
+### Title: Estimate the number of dimensions for the Multiple
+###   Correspondence Analysis by cross-validation
+### Aliases: estim_ncpMCA
+### Keywords: multivariate
+
+### ** Examples
+
+## Not run: 
+##D data(vnf)
+##D result <- estim_ncpMCA(vnf,ncp.min=0, ncp.max=3, nbsim=100)
+## End(Not run)
+
+
+
+cleanEx()
+nameEx("estim_ncpPCA")
+### * estim_ncpPCA
+
+flush(stderr()); flush(stdout())
+
+### Name: estim_ncpPCA
+### Title: Estimate the number of dimensions for the Principal Component
+###   Analysis by cross-validation
+### Aliases: estim_ncpPCA
+### Keywords: multivariate
+
+### ** Examples
+
+## Not run: 
+##D data(orange)
+##D nb <- estim_ncpPCA(orange,ncp.min=0,ncp.max=4) ## Time consuming, nb = 2
+## End(Not run)
+
+
+
+cleanEx()
+nameEx("imputeMCA")
+### * imputeMCA
+
+flush(stderr()); flush(stdout())
+
+### Name: imputeMCA
+### Title: Impute missing values in categorical variables with Multiple
+###   Correspondence Analysis
+### Aliases: imputeMCA
+### Keywords: models multivariate
+
+### ** Examples
+
+## Not run: 
+##D data(vnf)
+##D ## First the number of components has to be chosen 
+##D ##   (for the reconstruction step)
+##D ## nb <- estim_ncpMCA(vnf,ncp.max=5) ## Time-consuming, nb = 4
+##D 
+##D ## Impute indicator matrix and perform a MCA
+##D tab.disj.impute <- imputeMCA(vnf, ncp=4)$tab.disj
+##D res.mca <- MCA(vnf,tab.disj=tab.disj.impute)
+## End(Not run)
+
+
+
+cleanEx()
+nameEx("imputeMFA")
+### * imputeMFA
+
+flush(stderr()); flush(stdout())
+
+### Name: imputeMFA
+### Title: Impute dataset with MFA
+### Aliases: imputeMFA
+### Keywords: models multivariate
+
+### ** Examples
+
+data(orange)
+res.comp <- imputeMFA(orange,group=c(5,3),type=rep("s",2),ncp=2)
+## Note that MFA is performed on the completed matrix
+res.mfa <- MFA(res.comp$completeObs,group=c(5,3),type=rep("s",2))
+
+## Not run: 
+##D data(vnf)
+##D res.comp <- imputeMFA(vnf,group=c(6,5,3),type=c("n","n","n"),ncp=2)
+##D res.mfa <- MFA(vnf,group=c(6,5,3),type=c("n","n","n"),tab.comp=res.comp)
+## End(Not run)
+
+
+
+cleanEx()
+nameEx("imputePCA")
+### * imputePCA
+
+flush(stderr()); flush(stdout())
+
+### Name: imputePCA
+### Title: Impute dataset with PCA
+### Aliases: imputePCA
+### Keywords: models multivariate
+
+### ** Examples
+
+data(orange)
+## First the number of components has to be chosen 
+##   (for the reconstruction step)
+## nb <- estim_ncpPCA(orange,ncp.max=5) ## Time consuming, nb = 2
+
+## Imputation
+res.comp <- imputePCA(orange,ncp=2)
+
+## A PCA can be performed
+res.pca <- PCA(res.comp$completeObs)
+
+
+
+cleanEx()
+nameEx("orange")
+### * orange
+
+flush(stderr()); flush(stdout())
+
+### Name: orange
+### Title: Sensory description of 12 orange juices by 8 attributes.
+### Aliases: orange
+### Keywords: datasets
+
+### ** Examples
+
+data(orange)
+## Not run: 
+##D nb <- estim_ncpPCA(orange,ncp.min=0,ncp.max=5,method.cv="Kfold",nbsim=20,pNA=0.05)
+##D res.comp <- imputePCA(orange,ncp=nb$ncp)
+##D res.pca <- PCA(res.comp$completeObs)
+##D resMI <- MIPCA(orange,ncp=nb$ncp)
+##D plot(resMI)
+## End(Not run)
+
+
+
+cleanEx()
+nameEx("plot.MIPCA")
+### * plot.MIPCA
+
+flush(stderr()); flush(stdout())
+
+### Name: plot.MIPCA
+### Title: Plot the graphs for the Multiple Imputation in PCA
+### Aliases: plot.MIPCA
+### Keywords: dplot
+
+### ** Examples
+
+data(orange)
+## nb <- estim_ncpPCA(orange,ncp.max=5) ## Time consuming, nb = 2
+resMI <- MIPCA(orange,ncp=2)
+plot(resMI)
+
+
+
+cleanEx()
+nameEx("vnf")
+### * vnf
+
+flush(stderr()); flush(stdout())
+
+### Name: vnf
+### Title: Questionnaire done by 1232 individuals who answered 14 questions
+### Aliases: vnf
+### Keywords: datasets
+
+### ** Examples
+
+data(vnf)
+tab.disj.impute <- imputeMCA(vnf, ncp=4)$tab.disj
+res.mca <- MCA(vnf,tab.disj=tab.disj.impute)
+
+
+
+### * <FOOTER>
+###
+cat("Time elapsed: ", proc.time() - get("ptime", pos = 'CheckExEnv'),"\n")
+grDevices::dev.off()
+###
+### Local variables: ***
+### mode: outline-minor ***
+### outline-regexp: "\\(> \\)?### [*]+" ***
+### End: ***
+quit('no')
